c8b09e193fe9df44a593e56021b54aef1bb92d81
irinabech <irina.bech12@yandex.ru>
2022-04-10 20:42:53 +0700

    formData

--- src/index.js
+++ src/index.js
@@ -2,20 +2,37 @@ window.onload = function () {
     const link = document.getElementById("link");
     const menuBtn = document.getElementById("checkbox");
     const menuMob = document.querySelector(".mobile-menu__container");
+    const form = document.getElementById("form");
 
+    //Mobile menu
     menuBtn.onclick = function () {
         menuMob.classList.toggle("active");
     };
-
     link.onclick = function () {
         menuMob.classList.toggle("active");
     };
-};
 
-$(document).ready(function () {
-    $(".portfolio__box").slick({
-        slidesToShow: 4,
-        prevArrow: ".portfolio__btn_back",
-        nextArrow: ".portfolio__btn__next",
+    //Form data
+    form.addEventListener("submit", formData);
+
+    function formData(event) {
+        event.preventDefault();
+        const userName = form.querySelector('[name="name"]');
+        const phone = form.querySelector('[name="phone"]');
+        const userData = {
+            name: userName.value,
+            phone: phone.value,
+        };
+
+        console.log(userData);
+    }
+
+    //Slider
+    $(document).ready(function () {
+        $(".portfolio__box").slick({
+            slidesToShow: 4,
+            prevArrow: ".portfolio__btn_back",
+            nextArrow: ".portfolio__btn__next",
+        });
     });
-});
+};
