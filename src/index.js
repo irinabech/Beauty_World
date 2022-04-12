958e8c5dcd75ce1b48b285a6fcc5eb5799af19c0
irinabech <irina.bech12@yandex.ru>
2022-04-12 12:09:20 +0700

    Tabs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ window.onload = function () {
     const menuBtn = document.getElementById("checkbox");
     const menuMob = document.querySelector(".mobile-menu__container");
     const form = document.getElementById("form");
+    const priceBtn = document.querySelectorAll(".price__navigation ul>li>a");
+    const priceBox = document.querySelectorAll(".price__box");
+
+    //Tabs
+    for (let i = 0; i < priceBtn.length; i++) {
+        priceBtn[i].addEventListener("click", (e) => {
+            const tabsPath = e.target.dataset.tabsPath;
+            priceBtn.forEach((el) => {
+                el.classList.remove("navigation_active");
+            });
+            document
+                .querySelector(`[data-tabs-path="${tabsPath}"]`)
+                .classList.add("navigation_active");
+            tabsHandler(tabsPath);
+        });
+    }
+
+    const tabsHandler = (path) => {
+        priceBox.forEach((el) => {
+            el.classList.remove("price-box_active");
+        });
+        document
+            .querySelector(`[data-tabs-target="${path}"]`)
+            .classList.add("price-box_active");
+        priceBtn.forEach((btn) => {
+            btn.classList.remove("navigation_active-btn");
+        });
+        document
+            .querySelector(`[data-tabs-path="${path}"]`)
+            .classList.add("navigation_active-btn");
+    };
+
+    tabsHandler("Styling"); // Default state
 
     //Mobile menu
     menuBtn.onclick = function () {
