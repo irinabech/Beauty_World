54b995289d5a58cda433af2444ed78fdd687857f
irinabech <irina.bech12@yandex.ru>
2022-04-17 19:03:16 +0700

    Input mask

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import "slick-carousel/slick/slick";
 import "@fancyapps/fancybox/dist/jquery.fancybox";
 import "jquery-validation/dist/jquery.validate";
+import "inputmask/dist/jquery.inputmask";
 import "./main";
