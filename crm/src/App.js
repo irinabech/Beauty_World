bc31325766f0915bf87f529e9066657e47ec34b7
irinabech <irina.bech12@yandex.ru>
2022-04-26 17:40:14 +0700

    Router

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { LoginPage } from "./features/login/LoginPage";
+import { OrderPage } from "./features/orders/OrdersPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <h1>Welcome to React Router!</h1>
+
+            <ul>
+                <li>
+                    <Link to="/">Заявки</Link>
+                </li>
+                <li>
+                    <Link to="/login">Авторизация</Link>
+                </li>
+            </ul>
+
+            <Routes>
+                <Route path="/" element={<OrderPage />} />
+                <Route path="login" element={<LoginPage />} />
+            </Routes>
+        </div>
+    );
 }
-
-export default App;
